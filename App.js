1b7c6e6c8b27f72392081367bfde5ec567882198
Devendran <cmdevendran@gmail.com>
2026-08-20 16:59:33 +0530

    hardcoded /api

--- App.js
+++ App.js
@@ -9,10 +9,10 @@ import CartModal from './components/CartModal';
 // Clean decoupled import configuration profiles
 //import { API_BASE_URL } from './config'; 
 import {PORT} from './config'
-const API_BASE_URL =
-  process.env.NODE_ENV === 'development'
+const API_BASE_URL = '/api'
+/*   process.env.NODE_ENV === 'development'
     ? `http://localhost:5000/api`
-    : '/api';
+    : '/api'; */
 
 console.log("api base url "+API_BASE_URL)
 export default function App() {
